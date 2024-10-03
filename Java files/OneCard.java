ad45c2ec41d7b27432f630c47c61491bb5dceb3a
Ada <ajli@smith.edu>
2024-10-03 09:55:46 -0400

    Modified part of function in OneCard and created Student

---
+++ Java files/OneCard.java
@@ -0,0 +1,73 @@
+public class OneCard{
+
+    // Attributes
+    private Student owner;
+    private double balance;
+
+    //Constructor
+    public OneCard(Student s, double amt){
+        this.owner = s;
+        if (amt < 0) {
+            throw new RuntimeException("Cannot create a OneCard with a negative starting balance.");
+        }
+        this.balance = amt;
+    }
+
+    // Getters
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getOwner() {
+        return owner.getName();
+    }
+
+    // // Setter
+    // public void setOwner(String new_name) {
+    //     if (this.owner.getName() != new_name) {
+    //         this.owner = new_name;
+    //     } else {
+    //         throw new RuntimeException("Name unchanged.");
+    //     }
+    // }
+
+    // Methods
+    public void deposit(double amt) {
+        this.balance += amt;
+    }
+
+    /**
+     * Tests whether the balance on the card 
+     * is sufficient to cover a charge
+     * @param amt the desired charge
+     * @return T/F: whether the balance is sufficient
+     */
+    public boolean canAfford(double amt) {
+        return this.balance >= amt;
+    }
+
+    public void spend(double amt) {
+        if (this.canAfford(amt)) {
+            this.balance -= amt;
+        } else {
+            throw new RuntimeException("Low balance! Cannot afford " + amt);
+        }
+    }
+
+    public String toString() {
+        return this.owner + " has a balance of $" + Double.toString(this.balance);
+    }
+
+    public static void main(String[]args) {
+        Student me = new Student("Jordan");
+        OneCard myCard = new OneCard(me, 3.00);
+        try {
+            myCard.spend(10.);
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+        System.out.println(myCard);
+    }
+    
+
+}
---
+++ Java files/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String name;
+
+    public Student(String n){
+        this.name = n;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
