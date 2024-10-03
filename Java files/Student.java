7b94256b9a1a9e15882c29e038b5dbe2b0db399f
Ada <ajli@smith.edu>
2024-10-03 10:29:26 -0400

    Changed part of Student.java

--- Java files/Student.java
+++ Java files/Student.java
@@ -1,11 +1,17 @@
 public class Student {
     private String name;
+    private School schoolAttended;
 
-    public Student(String n){
+    public Student(String n, School s){
         this.name = n;
+        this.schoolAttended = s;
     }
 
     public String getName(){
         return this.name;
+    }
+
+    public School getSchool(){
+        return this.schoolAttended;
     }
 }
